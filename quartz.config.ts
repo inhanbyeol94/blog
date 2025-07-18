2fa95713b0cce67be30d6a22e9f0a265cac496da
inhanbyeol94 <inhanbyeol94@gmail.com>
2025-07-19 01:50:50 +0900

    -add test.md file to content/nest

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: false,
+      cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
         body: "Source Sans Pro",
