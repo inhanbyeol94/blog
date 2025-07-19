ad499d9ab235b5317c5607f18bdc2040b84c9647
inhanbyeol94 <inhanbyeol94@gmail.com>
2025-07-20 02:55:52 +0900

    Updated font size in explorer.scss for better visual consistency

--- quartz.config.ts
+++ quartz.config.ts
@@ -45,7 +45,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#d4d4d4",
+          secondary: "#A68AF9",
           tertiary: "#A68AF9",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
